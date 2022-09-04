65f2042c90b212ef15ce09671b72eb46ac3cc800
safaktepe <safaktepemert@gmail.com>
2022-09-04 19:32:11 +0300

    Initial Commit

---
+++ DCA-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DCA-Calculator
+//
+//  Created by Mert Şafaktepe on 4.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
